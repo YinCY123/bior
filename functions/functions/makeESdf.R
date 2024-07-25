e856fb1e26f7e74c12e8d8bc9d2e48e82803d64d
YinCY <yinchunyou2012@163.com>
2024-07-25 22:52:32 +0800

    remove

--- functions/functions/makeESdf.R
+++
@@ -1,7 +0,0 @@
-makeESdf <- function(es, features, join_by, ...){
-    mtx <- exprs(es)[features, ] %>% as.data.frame() %>% tibble::rownames_to_column("ID")
-    fdata <- fData(es)
-    df <- dplyr::left_join(mtx, fdata, by = join_by)
-    
-    return(df)
-}
